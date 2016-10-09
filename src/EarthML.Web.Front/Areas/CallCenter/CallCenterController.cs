c9d8e9664f3fbc7b5fe6f3186ddea4b768b66302
Poul Kjeldager Sørensen <pks@s-innovations.net>
2016-10-09 12:12:25 +0000

    PR 12: Created a callcenter

---
+++ src/EarthML.Web.Front/Areas/CallCenter/CallCenterController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EarthML.Web.Front.Areas.Home
+{
+    public class UserInputViewModel
+    {
+        public int Choice { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+    }
+    public class CallCenterController : Controller
+    {
+        [Route("[controller]")]
+        [Route("[controller]/Welcome")]
+        public IActionResult Welcome(string From =null, string To = null)
+        {
+            return View();
+        }
+
+        public IActionResult HandleUserInput(string digits, string From = null, string To = null)
+        {
+            int choice;
+           return View(new UserInputViewModel{ Choice = int.TryParse(digits, out choice)? choice : -1, From = From, To=To });
+           
+        }
+
+    }
+}
